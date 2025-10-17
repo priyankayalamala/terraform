ea1df2e7cfa5a00f5aae5e5d38381515e33f053f
priyanka <priyanka.yalamala1@gmail.com>
2025-10-17 15:33:49 +0200

    terraform files

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -16,6 +16,28 @@ resource "aws_instance" "terraform" {
       command = "echo Instance is destroyed"
       when    = destroy
     }
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo dnf install nginx -y",
+        "sudo systemctl start nginx"
+      ]
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo systemctl stop nginx",
+        "echo 'successfully stopped nginx server' "
+      ]
+      when = destroy
+    }
 }
 
 resource "aws_security_group" "allow_all" {
