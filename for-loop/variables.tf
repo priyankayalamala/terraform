5a0f28f47c267ec74c6459cde9c76cff29864408
priyanka <priyanka.yalamala1@gmail.com>
2026-01-11 05:47:03 +0100

    terraform multi env

--- for-loop/variables.tf
+++ for-loop/variables.tf
@@ -8,9 +8,9 @@ variable "instances" {
 }
 
 variable "zone_id" {
-    default = "Z05052331V4VEGNQW2KX2"
+    default = "Z011939620HFSB9Q65KTW"
     }
 
 variable "domain_name" {
-    default = "daws86s.shop"
+    default = "priyankadevops.shop"
     }
